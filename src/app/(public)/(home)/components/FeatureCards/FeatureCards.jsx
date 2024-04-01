01771cfe394fb140cde75ddff214a880e6d3c243
Saif-Tasnim <saiftasnim2002@gmail.com>
2024-04-01 19:39:28 +0600

    ci: slider added

--- src/app/(public)/(home)/components/FeatureCards/FeatureCards.jsx
+++ src/app/(public)/(home)/components/FeatureCards/FeatureCards.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import SingleCard from "./SingleCard";
+import FeatureSwiper from "./FeatureSwiper";
 
 const FeatureCards = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-20 mx-5 md:mx-0">
-      <SingleCard
-        heading="Affordable Auto Insurance "
-        para="USA Underwriters offers affordable rates on quality coverage."
-      />
+    <>
+      <div className="mt-20 hidden lg:block">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <SingleCard
+            heading="Affordable Auto Insurance "
+            para="USA Underwriters offers affordable rates on quality coverage."
+          />
 
-      <SingleCard
-        heading="Accidents Can Add Up"
-        para="USA Underwriters offers affordable rates on quality coverage.Accidents and infractions can linger long after the fines are paid and the car is repaired."
-      />
+          <SingleCard
+            heading="Accidents Can Add Up"
+            para="USA Underwriters offers affordable rates on quality coverage.Accidents and infractions can linger long after the fines are paid and the car is repaired."
+          />
 
-      <SingleCard
-        heading="Price Conscious"
-        para="Price conscious consumers choose USA Underwriters for affordable auto insurance solutions."
-      />
-    </div>
+          <SingleCard
+            heading="Price Conscious"
+            para="Price conscious consumers choose USA Underwriters for affordable auto insurance solutions."
+          />
+        </div>
+      </div>
+
+      <div className="mt-20 mx-5 block lg:hidden">
+         <FeatureSwiper />
+      </div>
+    </>
   );
 };
 
